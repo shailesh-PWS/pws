956098e96950a677cb76f3699fc002e684446bf1
shailesh-pws <shailesh@playwarestudios.com>
2020-05-18 20:40:33 +0800

    Removed ICA folders

--- ICACargo/Data/Games/Azamwm2051/Game/icacargoverage_export/GameMedia/javascript/utils/FontUtils.js
+++
@@ -1,9 +0,0 @@
-var FontUtils = new function(){
-    this.resize = function(id){
-        var curfontsize = $(id).css("font-size");
-        curfontsize = parseInt(curfontsize);
-        var fontscale = $("#minigameWrapper").height()/480;
-        var newfontsize = Math.round(curfontsize*fontscale);
-        $(id).css("font-size",newfontsize);
-    }
-}
